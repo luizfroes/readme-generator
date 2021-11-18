0be7087dee0c6935441bd58c44538c80462cdb31
Luiz Carlos Froes <luizfroes@gmail.com>
2021-11-18 02:52:31 +0000

    Added write function to util.js

--- src/util.js
+++ src/util.js
@@ -1,90 +1,4 @@
-const generateTitle = (answers) => {
-  return `# Title ![MIT](https://img.shields.io/static/v1?label=<MIT>&message=<License>&color=<Green>)
-    `;
-};
-
-const generateTableOfContents = (answers) => {
-  return `## Table of Contents
-      
-    - [Description](#description)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Tests](#tests)
-    - [Contributing](#contributing)
-    - [License](#license)`;
-};
-
-const generateDescription = (answers) => {
-  return `## Description
-      
-    DESCRIPTION`;
-};
-
-const generateInstallation = (answers) => {
-  return `## Installation
-      
-    Run the following script to instal the packages required for the application:
-      
-    \`\`\`
-    ADD TEXT HERE
-    \`\`\``;
-};
-
-const generateUsage = (answers) => {
-  return `## Usage
-      
-    To use the application run the following script:
-      
-    \`\`\`
-    ADD TEXT HERE
-    \`\`\``;
-};
-
-const generateTests = (answers) => {
-  return `## Tests
-      
-    To use the application run the following script:
-      
-    \`\`\`
-    ADD TEXT HERE
-    \`\`\``;
-};
-
-const generateContributing = (answers) => {
-  return `## Contributing
-  
-    ADD TEXT HERE
-      
-    If you would like more info about how to contribute in this application, please fell free to get in touch with me by:
-  
-    Email: ADD TEXT HERE
-    GitHub: ADD TEXT HERE`;
-};
-
-const generateLicense = (answers) => {
-  return `## License
-      
-    ADD TEXT HERE`;
-};
-
-const generateReadme = (answers) => {
-  return `${generateTitle(answers)}
-  
-    ${generateTableOfContents(answers)}
-      
-    ${generateDescription(answers)}
-      
-    ${generateInstallation(answers)}
-      
-    ${generateUsage(answers)}
-      
-    ${generateTests(answers)}
-      
-    ${generateContributing(answers)}
-      
-    ${generateLicense(answers)}
-      `;
-};
+const fs = require("fs");
 
 // write to file
 const writeToFile = (filePath, data) => {
@@ -93,4 +7,8 @@ const writeToFile = (filePath, data) => {
   } catch (error) {
     console.log(error.message);
   }
+};
+
+module.exports = {
+  writeToFile,
 };
